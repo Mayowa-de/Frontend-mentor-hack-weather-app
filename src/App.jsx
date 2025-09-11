a63ade931c11480279bde2317f6c53aa5113722f
Mayowa-de <mayortech03@gmail.com>
2025-09-11 06:20:50 -0700

    resolving the issue by aligning the components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import './index.css'
-import Header from "./Hero/Header.jsx"
-import Title from "./Hero/title"
-import TodayCloud from './Hero/todayCloud'
-import SubCloud from './Hero/SubCloud.jsx'
-import HourCast from './Hero/HourCast.jsx'
+import "./index.css";
+import Header from "./Hero/Header.jsx";
+import Title from "./Hero/title";
+import TodayCloud from "./Hero/todayCloud";
+import SubCloud from "./Hero/SubCloud.jsx";
+import HourCast from "./Hero/HourCast.jsx";
 
 function App() {
-
   return (
     <>
-    <div className=' items-center flex mb-10 flex-col pb-10 px-24 '>
-    <Header/>
-    <Title/>
-    <div className='flex gap-5'>
-    <TodayCloud/>
-    <HourCast/>
-    </div>
-    <SubCloud/>
-    </div>
+      <div className=" items-center flex mb-10 flex-col pb-10 px-24 ">
+        <Header />
+        <Title />
+        <div className="flex gap-5">
+          <div className="flex flex-col">
+            <TodayCloud />
+            <SubCloud />
+          </div>
+          <HourCast />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
