fe0f5d4ab8aefade36be00cb088ab1501d03cb3e
Mayowa-de <mayortech03@gmail.com>
2025-09-11 09:41:09 -0700

    duplicated the first card to create other card

--- src/Hero/DailyForeCast.jsx
+++ src/Hero/DailyForeCast.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import RainIcon  from '../assets/images/icon-rain.webp'
 export default function DailyForeCast() {
   return (
-    <div className='flex mt-4 text-white'>
-      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2'>
+    <div className='flex flex-col mt-7 text-white gap-4'>
+        <h3 className='text-2xl font-sans font-semibold'>Daily forecast</h3>
+         <div className='flex  gap-3'>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
         <h5>Tue</h5>
         <img src={RainIcon} alt="" className='w-16'/>
         <div className='flex gap-5 w-full'>
@@ -13,6 +15,79 @@ export default function DailyForeCast() {
             <div className='flex  justify-end w-full'>
             <p>14</p><span className='-mt-1 text-[12px]'>o</span>
             </div>
+        </div>
+        </div>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
+        <h5>Tue</h5>
+        <img src={RainIcon} alt="" className='w-16'/>
+        <div className='flex gap-5 w-full'>
+            <div className='flex '>
+            <p>20</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+            <div className='flex  justify-end w-full'>
+            <p>14</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+        </div>
+        </div>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
+        <h5>Tue</h5>
+        <img src={RainIcon} alt="" className='w-16'/>
+        <div className='flex gap-5 w-full'>
+            <div className='flex '>
+            <p>20</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+            <div className='flex  justify-end w-full'>
+            <p>14</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+        </div>
+        </div>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
+        <h5>Tue</h5>
+        <img src={RainIcon} alt="" className='w-16'/>
+        <div className='flex gap-5 w-full'>
+            <div className='flex '>
+            <p>20</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+            <div className='flex  justify-end w-full'>
+            <p>14</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+        </div>
+        </div>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
+        <h5>Tue</h5>
+        <img src={RainIcon} alt="" className='w-16'/>
+        <div className='flex gap-5 w-full'>
+            <div className='flex '>
+            <p>20</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+            <div className='flex  justify-end w-full'>
+            <p>14</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+        </div>
+        </div>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
+        <h5>Tue</h5>
+        <img src={RainIcon} alt="" className='w-16'/>
+        <div className='flex gap-5 w-full'>
+            <div className='flex '>
+            <p>20</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+            <div className='flex  justify-end w-full'>
+            <p>14</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+        </div>
+        </div>
+      <div className='card flex flex-col gap-5 bg-card w-28 items-center justify-center  rounded-xl p-2 shadow-xl'>
+        <h5>Tue</h5>
+        <img src={RainIcon} alt="" className='w-16'/>
+        <div className='flex gap-5 w-full'>
+            <div className='flex '>
+            <p>20</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+            <div className='flex  justify-end w-full'>
+            <p>14</p><span className='-mt-1 text-[12px]'>o</span>
+            </div>
+        </div>
         </div>
       </div>
     </div>
