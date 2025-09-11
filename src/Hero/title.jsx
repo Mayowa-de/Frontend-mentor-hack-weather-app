8581152534b6a0bfb43a50a01b2dbf6e2e2e667f
Mayowa-de <mayortech03@gmail.com>
2025-09-11 06:48:22 -0700

    add to the between the title and  search

--- src/Hero/title.jsx
+++ src/Hero/title.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import SearchIcon from '../assets/images/icon-search.svg'
+import React from "react";
+import SearchIcon from "../assets/images/icon-search.svg";
 
 export default function title() {
   return (
-    <div className='justify-center flex items-center flex-col gap-5 mt-20 max-w-full'>
-      <h1 className='text-white text-4xl w-full font-bold'>How's the sky looking today?</h1>
-      <div className='flex gap-3 '>
-        <div className='flex items-center gap-0 '>
-      <img src={SearchIcon} className='  w-4  ml-4 absolute ' alt="" /><input type="text" placeholder='Search for a place' className='w-[350px] h-10 px-10 text-sm border-none rounded-md text-white outline-none bg-secondary placeholder:text-grey-300'/>
-      </div>
-      <button className='btn font-DMSans bg-button border-none shadow-none text-white'>Search</button>
+    <div className="justify-center flex items-center flex-col gap-20 mt-20 max-w-full">
+      <h1 className="text-white text-5xl w-full font-bold">
+        How's the sky looking today?
+      </h1>
+      <div className="flex gap-3 ">
+        <div className="flex items-center gap-0 ">
+          <img src={SearchIcon} className="  w-4  ml-4 absolute pointer-events-none" alt="" />
+          <input
+            type="text"
+            placeholder="Search for a place"
+            className="w-[350px] h-10 px-10 text-sm border-none rounded-md text-white outline-none bg-secondary placeholder:text-grey-300"
+          />
+        </div>
+        <button className="btn font-DMSans bg-button border-none shadow-none text-white">
+          Search
+        </button>
       </div>
     </div>
-  )
+  );
 }
