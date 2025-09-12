c1f1cf7b8c8854b35b9b84a221a11cbb8c78909c
Mayowa-de <mayortech03@gmail.com>
2025-09-12 04:21:28 -0700

    adjust font and text size

--- src/Hero/SubCloud.jsx
+++ src/Hero/SubCloud.jsx
@@ -4,27 +4,27 @@ export default function SubCloud() {
   return (
     <div className="flex gap-4 justify-start  w-full font-Inter">
       <div className="flex flex-col gap-4 bg-card text-white rounded-md p-5 w-full">
-        <h5 className="text-xl text-borderColor">Feels like</h5>
+        <h5 className="text-base font-semibold text-borderColor">Feels like</h5>
         <div className="flex">
           <h1 className="text-4xl">18</h1>
-          <span className="-mt-3 text-2 flex">o C</span>
+          <span className="-mt-2 text-2 flex">o </span>
         </div>
       </div>
       <div className="flex flex-col gap-4 bg-card text-white rounded-md  p-5 w-full font-Inter">
-        <h5 className="text-xl text-borderColor">Humidity</h5>
+        <h5 className="text-base font-semibold text-borderColor">Humidity</h5>
         <div className="flex">
           <h1 className="text-4xl">46%</h1>
         </div>
       </div>
       <div className="flex flex-col gap-4 bg-card text-white rounded-md  p-5 w-full font-Inter">
-        <h5 className="text-xl text-borderColor">Wind</h5>
+        <h5 className="text-base font-semibold text-borderColor">Wind</h5>
         <div className="flex">
           <h1 className="text-4xl font-Inter">14</h1>
           <span className="text-3xl flex  ml-2 font-Inter">Km/h</span>
         </div>
       </div>
       <div className="flex flex-col gap-4 bg-card text-white rounded-md p-5 w-full font-Inter">
-        <h5 className="text-xl text-borderColor">Precipitation</h5>
+        <h5 className="text-base font-semibold text-borderColor">Precipitation</h5>
         <div className="flex">
           <h1 className="text-4xl font-Inter">0</h1>
           <span className="text-2xl mt-2 ml-2 flex text-Inter">mm</span>
