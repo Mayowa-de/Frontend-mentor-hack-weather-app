7fd8778bc2d44de0435395a6e79c287191eca094
Mayowa-de <mayortech03@gmail.com>
2025-09-12 17:46:20 -0700

    tracking the state of the search and causing an event from button

--- src/Hero/title.jsx
+++ src/Hero/title.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "../assets/images/icon-search.svg";
 
-export default function title() {
+export default function title({onSearch}) {
+  const [location, setLocation] =useState('')
+
+  const handleSearch = () =>{
+    onSearch(location)
+  }
   return (
     <div className="justify-center flex items-center flex-col gap-20 mt-20 max-w-full">
       <h1 className="text-white text-5xl w-full font-bold">
@@ -12,11 +17,12 @@ export default function title() {
           <img src={SearchIcon} className="  w-4  ml-4 absolute pointer-events-none" alt="" />
           <input
             type="text"
+            onChange={(e)=>setLocation(e.target.value)}
             placeholder="Search for a place"
             className="w-[450px] h-12 px-10 text-sm border-none rounded-md text-white outline-none bg-secondary placeholder:text-grey-300"
           />
         </div>
-        <button className="btn  bg-button border-none shadow-none text-white w-24 h-12">
+        <button onClick={handleSearch} className="btn  bg-button border-none shadow-none text-white w-24 h-12">
           Search
         </button>
       </div>
