d55dda965d4e0a0dc8077b7da071707263fc39c1
Mayowa-de <mayortech03@gmail.com>
2025-09-12 17:10:51 -0700

    resolve error of calling weather code function

--- src/Hero/todayCloud.jsx
+++ src/Hero/todayCloud.jsx
@@ -6,13 +6,13 @@ import loadingIcon from "../assets/images/icon-loading.svg"
 import "./Hero.css";
 
 const getWeatherIcon = (code) =>{
-  if([0,1].includes(code)) return  '../assets/images/icon-sunny.webp'
-  if([2,3].includes(code)) return  '../assets/images/icon-partly-cloudy.webp'
-  if([45,48].includes(code)) return  '../assets/images/icon-fog.webp'
-  if([51,61,63,65].includes(code)) return  '../assets/images/icon-drizzle.webp'
-  if([71,73,75,77].includes(code)) return  '../assets/images/icon-snow.webp'
-  if([95,96,99].includes(code)) return  '../assets/images/icon-storm.webp'
-  if([63].includes(code)) return '../assets/images/icon-rain.webp'
+  if([0,1].includes(code)) return  require('../assets/images/icon-sunny.webp')
+  if([2,3].includes(code)) return  require('../assets/images/icon-partly-cloudy.webp')
+  if([45,48].includes(code)) return  require('../assets/images/icon-fog.webp')
+  if([51,61,63,65].includes(code)) return  require('../assets/images/icon-drizzle.webp')
+  if([71,73,75,77].includes(code)) return  require('../assets/images/icon-snow.webp')
+  if([95,96,99].includes(code)) return  require('../assets/images/icon-storm.webp')
+  if([63].includes(code)) return require('../assets/images/icon-rain.webp')
 }
 export default function todayCloud() {
   const [weather, setWeather] = useState(null);
@@ -34,7 +34,7 @@ export default function todayCloud() {
 
     if(!weather) return <img src={loadingIcon} className="w-16 h-16 animate-rotate  justify-center flex items-center ml-96 p-4 pt-6"/>
 
-   const iconFile= getWeatherIcon[weather.weathercode] || 'default.webp';
+   const iconFile= getWeatherIcon(weather.weathercode) || require('default.webp');
 
     const date = new Date()
     const currentYear= date.getFullYear()
@@ -51,7 +51,7 @@ export default function todayCloud() {
           <p>{day}, {month} {dayNumber}, {currentYear}</p>
         </div>
         <div className="flex justify-end gap-12 todayCloud mr-52 items-center">
-          <img src={`${iconFile}`} alt="" className="w-36  -ml-40 -mt-3" />
+          <img src={iconFile} alt="" className="w-36  -ml-40 -mt-3" />
           <div className="flex gap-5 italic ">
             <h1 className="text-8xl font-DMSans-Italic text-end text-white font-bold">
               {weather.temperature}
