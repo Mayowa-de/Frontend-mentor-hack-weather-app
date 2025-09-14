f542fa3820a3a8f127a2770c65e3894d36345465
Mayowa-de <mayortech03@gmail.com>
2025-09-14 11:13:16 -0700

    adjust the input font-size

--- src/Hero/title.jsx
+++ src/Hero/title.jsx
@@ -20,8 +20,8 @@ export default function title({onSearch}) {
           <input
             type="text"
             onChange={(e)=>setLocation(e.target.value)}
-            placeholder="Search for a place"
-            className="md:w-[450px] w-full h-12 px-10 text-sm border-none rounded-md text-white outline-white outline-1 bg-secondary placeholder:text-grey-300"
+            placeholder="Search for a place..."
+            className="md:w-[450px] w-full h-12 px-10 text-xl border-none rounded-md text-white outline-white outline-1 bg-secondary placeholder:text-grey-300"
           />
         </div>
         <button onClick={handleSearch} className="btn font-normal  bg-button border-none shadow-none text-white md:w-24 w-full h-12 text-xl">
