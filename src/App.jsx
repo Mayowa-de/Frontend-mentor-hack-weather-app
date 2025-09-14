d2baf2b2181cec296e4422e2d75600d46111d821
Mayowa-de <mayortech03@gmail.com>
2025-09-14 08:51:34 -0700

    fetch  the data and location of the input in  search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import Header from "./Hero/Header.jsx";
 import Title from "./Hero/title";
@@ -6,6 +6,7 @@ import TodayCloud from "./Hero/todayCloud";
 import SubCloud from "./Hero/SubCloud.jsx";
 import HourCast from "./Hero/HourCast.jsx";
 import DailyForeCast from "./Hero/DailyForeCast.jsx";
+import { fetchWeatherData } from "./Hero/api.jsx";
 
 function App() {
   const [unit, setUnit] = useState('Celsius (C)');
@@ -14,17 +15,33 @@ const [selected, setSelected] = useState({
   wind: 'Km/h',
   precipitation: 'Millimeters (mm)'
 });
+const [weatherData, setWeatherData] = useState(null)
+const [coordinate, setCoordinate] =  useState({latitude: 0, longitude: 0})
 
+useEffect(()=>{
+  navigator.geolocation.getCurrentPosition((position)=>{
+    setCoordinate({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    })
+  if (coordinate.latitude !== 0 && coordinate.longitude !== 0) {
+    fetchWeatherData(coordinate, selected)
+      .then(data => setWeatherData(data))
+      .catch(err => console.error(err));
+  }
+
+  })
+}, [coordinate, selected])
 // Pass as props
 
   return (
     <>
       <div className=" items-center flex mb-10 flex-col pb-10 px-24 ">
         <Header unit={unit} setUnit={setUnit} selected={selected} setSelected={setSelected} />
-        <Title />
+        <Title  />
         <div className="flex gap-7">
           <div className="flex flex-col ">
-            <TodayCloud />
+            <TodayCloud  data={weatherData}/>
             <SubCloud />
             <DailyForeCast/>
           </div>
