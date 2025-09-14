6a6d21de18579d2fd1bc7f1d07245e336fb248b0
Mayowa-de <mayortech03@gmail.com>
2025-09-14 10:14:28 -0700

    work on the mobile view of width

--- src/Hero/todayCloud.jsx
+++ src/Hero/todayCloud.jsx
@@ -56,7 +56,7 @@ export default function todayCloud({data}) {
   return (
     <div className="justify-center  mt-10 mb-5">
       <img src={Bgicon} alt="" className="h-80" />
-      <div className="z-10 top-[460px] justify-between flex px-7 absolute items-center w-3/4">
+      <div className="z-10 top-[460px] justify-between flex px-7 absolute items-center w-full md:w-3/4">
         <div className="flex flex-col gap-3 text-white/90">
           <h3 className="text-3xl">France</h3>
           <p>{day}, {month} {dayNumber}, {currentYear}</p>
