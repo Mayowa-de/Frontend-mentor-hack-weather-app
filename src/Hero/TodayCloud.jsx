fc2749c4c866e472170383a0ab7b54916db54990
Mayowa-de <mayortech03@gmail.com>
2025-09-15 06:47:35 -0700

    adjust the width of loading state

--- src/Hero/TodayCloud.jsx
+++ src/Hero/TodayCloud.jsx
@@ -22,9 +22,9 @@ export default function TodayCloud({data}) {
   
     if (!data || !data.current_weather.temperature) {
     return (
-      <div className="bg-secondary text-white/90  md:w-[850px] w-full h-72 justify-center items-center flex m-12 mx-0 rounded-2xl">
-        <img src={loadingIcon} className="w-20 h-20 animate-rotate  justify-center flex items-center"/>
-        <p className="-ml-20 z-20">Loading...</p>
+      <div className="bg-secondary text-white/90  md:w-[800px] w-full h-72 justify-center items-center flex flex-col m-12 mx-0 rounded-2xl">
+        <img src={loadingIcon} className="w-10 h-20 animate-rotate  justify-center flex items-center"/>
+        <p className="-ml-1 ">Loading...</p>
       </div>
     );
   }
