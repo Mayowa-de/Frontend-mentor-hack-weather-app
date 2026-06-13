642e246dfcb7babb6535074d0b01df340368006c
Mayowa-de <mayortech3@gmail.com>
2026-06-13 15:01:30 +0100

    Fix URL construction for weather API request

--- src/Hero/api.jsx
+++ src/Hero/api.jsx
@@ -41,7 +41,7 @@ export const fetchWeatherData = async (
   // Fetch weather data
   const unit =
     selected.temperature === "Fahrenheit (F)" ? "fahrenheit" : "celsius";
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&temperature_unit=${unit}&timezone=auto&hourly=temperature_2m,weathercode,relative_humidity_2m,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum,windspeed_10m_max&current=relative_humidity_2m&forecast_days=7`;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_unit=${unit}&timezone=auto&hourly=temperature_2m,weathercode,relative_humidity_2m,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum,windspeed_10m_max&current=relative_humidity_2m&forecast_days=7`;
   const response = await fetch(url);
 
 if (!response.ok) {
